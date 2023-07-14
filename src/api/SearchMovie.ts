545cd0cb6fb0b44fbcbe9d8e67f3475244da3c4b
adamni21 <85580454+adamni21@users.noreply.github.com>
2023-07-14 19:15:26 +0100

    SearchMovie query

---
+++ src/api/SearchMovie.ts
@@ -0,0 +1,35 @@
+import { kyd } from "./kyd";
+
+interface ResponseBody {
+  posts: Post[];
+}
+
+interface Post {
+  title: "string";
+  custom_fields: {
+    "IMDb-Link": string[];
+  };
+}
+
+const url =
+  "https://api.netzkino.de.simplecache.net/capi-2.0a/search?q=hitchcock&d=devtest";
+
+export const SearchMovies = async (
+  searchValue: string,
+  signal?: AbortSignal
+) => {
+  const json: ResponseBody = await kyd
+    .get(url, {
+      searchParams: {
+        q: searchValue,
+        d: import.meta.env.VITE_NETZKINO_KEY as string,
+      },
+      signal,
+    })
+    .json();
+
+  return json.posts.map((movie) => ({
+    title: movie.title,
+    imdbId: movie.custom_fields["IMDb-Link"][0].split("/").slice(-1)[0],
+  }));
+};
