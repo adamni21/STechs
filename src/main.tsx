c18b071739eb7610804d2c5e360b470ed5d086b8
adamni21 <85580454+adamni21@users.noreply.github.com>
2023-07-15 15:39:57 +0100

    increased cache time of QueryClient

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App.tsx";
 import "./index.css";
 
+// increase cache time as the data should be pretty static
+//                hh   mm   ss     ms
+const cacheTime = 24 * 60 * 60 * 1000;
+
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries: { refetchOnMount: false, refetchOnWindowFocus: false },
+    queries: { refetchOnMount: false, refetchOnWindowFocus: false, cacheTime },
   },
 });
 
