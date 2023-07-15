faa9fa89039cb693161d0c39e36b1f389b0c336b
adamni21 <85580454+adamni21@users.noreply.github.com>
2023-07-15 15:38:54 +0100

    GetMovieImage query

---
+++ src/api/GetMovieImage.ts
@@ -0,0 +1,33 @@
+import { kyd } from "./kyd";
+
+interface ResponseBody {
+  poster_path: string;
+}
+
+const url = "https://api.themoviedb.org/3/find/";
+
+const api_key = import.meta.env.VITE_TMDB_KEY as string;
+const language = "en-US";
+const external_source = "imdb_id";
+
+export const GetMovieImage = async (imdbId: string, signal?: AbortSignal) => {
+  const json: ResponseBody = await kyd
+    .get(url + imdbId, {
+      searchParams: {
+        api_key,
+        language,
+        external_source,
+      },
+      signal,
+    })
+    .json();
+
+  return json.poster_path;
+};
+
+type Size = "w92" | "w154" | "w185" | "w342" | "w500" | "w780";
+
+const imageBaseUrl = "http://image.tmdb.org/t/p";
+
+export const imageUrl = (path: string, size: Size) =>
+  `${imageBaseUrl}/${size}/${path}`;
