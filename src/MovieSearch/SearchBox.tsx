41b7b6b9044df8c9f15129fc52dffbf436f006d0
adamni21 <85580454+adamni21@users.noreply.github.com>
2023-07-16 16:54:47 +0100

    SearchBox

---
+++ src/MovieSearch/SearchBox.tsx
@@ -0,0 +1,21 @@
+import { ChangeEventHandler } from "react";
+
+interface Props {
+  isLoading: boolean;
+  value: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+}
+
+const SearchBox = ({ isLoading, value, onChange }: Props) => (
+  <div className="bg-slate-900 w-full py-5 mb-5 text-center sticky top-0 shadow-lg shadow-slate-500/20 rounded-lg">
+    <p className="mb-3 text-slate-100 text-xl font-bold">Search for movies</p>
+    <input
+      className="max-w-xl md:w-full lg:w-3/4 p-2 text-slate-100 bg-slate-800 border-2 border-slate-900 rounded-md"
+      value={value}
+      onChange={onChange}
+    />
+    {isLoading && <p className="text-slate-100 text-xl">LOADING!...</p>}
+  </div>
+);
+
+export default SearchBox;
