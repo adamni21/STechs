1a495c691382055f96ecc4b22cf74fdaa9482d0e
adamni21 <85580454+adamni21@users.noreply.github.com>
2023-07-16 21:52:55 +0100

    moved QueryClientProvider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import MovieSearch from "./MovieSearch/MovieSearch";
+
+// increase cache time as the data should be pretty static
+//                hh   mm   ss     ms
+const cacheTime = 24 * 60 * 60 * 1000;
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { refetchOnMount: false, refetchOnWindowFocus: false, cacheTime },
+  },
+});
 
 function App() {
   return (
-    <main className="bg-slate-900">
-      <MovieSearch />
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className="bg-slate-900">
+        <MovieSearch />
+      </main>
+    </QueryClientProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App.tsx";
 import "./index.css";
 
-// increase cache time as the data should be pretty static
-//                hh   mm   ss     ms
-const cacheTime = 24 * 60 * 60 * 1000;
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: { refetchOnMount: false, refetchOnWindowFocus: false, cacheTime },
-  },
-});
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
