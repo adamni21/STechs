ad17a12368b1be77deb78985ec25daf00693dee7
adamni21 <85580454+adamni21@users.noreply.github.com>
2023-07-18 08:31:17 +0100

    correct return type for mock

--- src/hooks/__mocks__/useIntersectedViewport.ts
+++ src/hooks/__mocks__/useIntersectedViewport.ts
@@ -1,5 +1,11 @@
 import { vi } from "vitest";
+import * as M from "../useIntersectedViewport";
 
-const useIntersectedViewport = vi.fn(() => true);
+const mocked: typeof M.default = () => ({
+  hasIntersected: true,
+  ref: { current: null },
+});
+
+const useIntersectedViewport = vi.fn(mocked);
 
 export default useIntersectedViewport;
